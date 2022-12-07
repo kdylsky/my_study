b377cc1706d338985649b4bdc6b326d5430a71a4
kdylsky <kdy9kdy@gmail.com>
2022-12-07 22:38:54 +0900

    [study] 도커 볼륨& 바인드마운트

--- docker_study/music/views.py
+++ docker_study/music/views.py
@@ -10,7 +10,7 @@ class MusicView(APIView):
     def get(self, request):
         obj = Music.objects.all()
         data = MusicSerialzier(instance=obj, many=True)
-        f = open('./sentence/text', 'r')
+        f = open('./sentence/test.txt', 'r')
         sentence = f.readlines()
         f.close()
         return JsonResponse({"sentence":sentence, "data":data.data}, status=status.HTTP_200_OK, safe=False)
@@ -20,7 +20,7 @@ class MusicView(APIView):
         params = MusicSerialzier(data=data)
         params.is_valid(raise_exception=True)
         params.save()
-        f = open('./sentence/text', 'a')
-        f.write(request.data.get("title") + " what " +request.data.get("author"))
+        f = open('./sentence/test.txt', 'a')
+        f.write(request.data.get("title") + " what and asdasd ?????? " +request.data.get("author"))
         f.close()
         return JsonResponse(params.data, status=status.HTTP_200_OK)
